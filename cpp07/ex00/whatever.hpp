5ba72499145e5a1d1835e1a0b47bcd0555d2415a
yugurlu <yusufugurlu39@outlook.com>
2023-07-20 12:57:41 +0300

    s

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,28 @@
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+T   max(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+template <typename T>
+T   min(T x, T y)
+{
+    return (x < y) ? x : y;
+}
+
+template <typename T>
+void   swap(T &x, T &y)
+{
+    T  temp = x;
+
+    x = y;
+    y = temp;
+}
+
+#endif
---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void   iter(T &array, T length, void(*func)(T))
+{
+    
+} 
+
+
+#endif
