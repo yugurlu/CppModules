1ca64fc9ff3604a94b39d30199d8f977f3d14aea
yugurlu <yusufugurlu39@outlook.com>
2023-07-20 17:49:54 +0300

    cpp07 finish

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,16 +1,26 @@
 
 #include "iter.hpp"
 
-void    func(int str)
+class Awesome
 {
-    cout << str << endl;
-}
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
 
-int main()
-{
-    int *arr = new int[3];
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-    ::iter(arr, 3, func);
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; return; }
+
+int main() {
+    int tab[] = { 0, 1, 2, 3, 4 };  // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+    Awesome tab2[5];
+
+    iter( tab, 5, print<int> );
+    iter( tab2, 5, print<Awesome> );
+
+    return 0;
 }
--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -9,11 +9,10 @@ using std::end;
 using std::string;
 
 template <class T>
-
 class   Array
 {
     private:
-        int len;
+        unsigned int len;
         T *elements;
     public:
         Array()
@@ -21,23 +20,36 @@ class   Array
             this->len = 0;
             this->elements = new T[this->n];
         }
-        Array(unsigned int n)
+        ~Array()
         {
-            this->len = n;
-            this->elements = new T[n];
+            delete this->elements;
+        }
+        Array(unsigned int len)
+        {
+            this->len = len;
+            this->elements = new T[this->len];
         }
         Array(const Array &copy)
         {
+            this->len = copy.len;
             this->elements = new T[copy.len];
-            for (int i = 0; i < copy.len; i++)
+            for (unsigned int i = 0; i < copy.len; i++)
                 this->elements[i] = copy.elements[i];
         }
         Array   &operator = (const Array &copy)
         {
+            this->len = copy.len;
             this->elements = new T[copy.len];
             for (int i = 0; i < copy.len; i++)
                 this->elements[i] = copy.elements[i];
             return *this;
+        }
+        T &operator[](unsigned  int index)
+        {
+            if(index >= this->len)
+                throw std::out_of_range("Index out of range");
+            else
+                return this->elements[index];
         }
 };
 
