586468167f225ca2b7ba505bbc32743d97adeb67
yugurlu <yusufugurlu39@outlook.com>
2023-07-30 19:53:58 +0300

    cpp08 finish

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,43 @@
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+template    <class T>
+class   MutantStack : public std::stack<T>
+{
+    public:
+        
+        MutantStack() {};
+        ~MutantStack() {};
+        MutantStack(const MutantStack &copy)
+        {
+            *this = copy;
+        }
+        MutantStack &operator = (const MutantStack &copy)
+        {
+            if (this != &copy)
+                *this = copy;
+            return *this;
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator    begin()
+        {
+            return this->c.begin();
+        }
+        iterator    end()
+        {
+            return this->c.end();
+        }
+};
+
+#endif
