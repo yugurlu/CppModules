9af152866927752b1f056af934c50332cc5ce00e
yugurlu <yusufugurlu39@outlook.com>
2023-09-18 12:50:05 +0300

    fix

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,48 +5,49 @@
 #include <iostream>
 
 using std::cout;
-using std::end;
+using std::endl;
 using std::string;
 
 template <class T>
 class   Array
 {
     private:
-        unsigned int len;
+        unsigned int n;
         T *elements;
     public:
         Array()
         {
-            this->len = 0;
+            this->n = 0;
+            cout << this->n << endl;
             this->elements = new T[this->n];
         }
         ~Array()
         {
             delete this->elements;
         }
-        Array(unsigned int len)
+        Array(unsigned int n)
         {
-            this->len = len;
-            this->elements = new T[this->len];
+            this->n = n;
+            this->elements = new T[this->n];
         }
         Array(const Array &copy)
         {
-            this->len = copy.len;
-            this->elements = new T[copy.len];
-            for (unsigned int i = 0; i < copy.len; i++)
+            this->n = copy.n;
+            this->elements = new T[copy.n];
+            for (unsigned int i = 0; i < copy.n; i++)
                 this->elements[i] = copy.elements[i];
         }
         Array   &operator = (const Array &copy)
         {
-            this->len = copy.len;
-            this->elements = new T[copy.len];
-            for (int i = 0; i < copy.len; i++)
+            this->n = copy.n;
+            this->elements = new T[copy.n];
+            for (int i = 0; i < copy.n; i++)
                 this->elements[i] = copy.elements[i];
             return *this;
         }
         T &operator[](unsigned  int index)
         {
-            if(index >= this->len)
+            if(index >= this->n)
                 throw std::out_of_range("Index out of range");
             else
                 return this->elements[index];
