c526f8d01251bb35535f700f9dccd867cea7858f
yugurlu <yusufugurlu39@outlook.com>
2023-09-18 13:13:45 +0300

    finishhhhh

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -1,6 +1,31 @@
 
 #include "whatever.hpp"
 
+class Awsome
+{
+    private:
+        int _n;
+    public:
+        Awsome(void) : _n(0) {}
+        Awsome(int n): _n(n) {}
+        Awsome & operator = (const Awsome & a) { _n = a._n; return *this; }
+        bool operator == (const Awsome & rhs) const { return this->_n == rhs._n; }
+        bool operator != (const Awsome & rhs) const { return this->_n != rhs._n; }
+        bool operator > (const Awsome & rhs) const { return this->_n > rhs._n; }
+        bool operator < (const Awsome & rhs) const { return this->_n < rhs._n; }
+        bool operator >= (const Awsome & rhs) const { return this->_n >= rhs._n; }
+        bool operator <= (const Awsome & rhs) const { return this->_n <= rhs._n; }
+
+        int get_n() const { return _n; }
+};
+
+std::ostream & operator << (std::ostream & o, const Awsome &a) { o << a.get_n(); return o; }
+
+
+
+
+
+
 int main( void )
 {
         int a = 2;
@@ -15,5 +40,13 @@ int main( void )
         std::cout << "c = " << c << ", d = " << d << std::endl;
         std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
         std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+        //-------------------------------------------------------------------------------------------------//
+
+        Awsome test(2), test2(4);
+        swap(test, test2);
+        std::cout << "test = " << test << ", test2 = " << test2 << std::endl;
+        std::cout << max(test, test2) << std::endl;
+        std::cout << min(test, test2) << std::endl;
         return 0;
 }
